80003d15e22ba52f9b930705e5a49d3a486d0edc
Joe Maples <frap129@gmail.com>
2015-09-12 10:47:15 -0400

    RadonX Bringup

--- core/config.mk
+++ core/config.mk
@@ -618,6 +618,6 @@ RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcomp
 
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/aosparadox/sepolicy/sepolicy.mk)
+$(eval include vendor/rnx/sepolicy/sepolicy.mk)
 
 include $(BUILD_SYSTEM)/dumpvar.mk
